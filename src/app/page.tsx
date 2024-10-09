ff511dbcc2dca01e75b84e6e28625a7d48df0f3f
DurgeshY21 <durgeshy5226@gmail.com>
2024-10-09 10:46:22 +0530

    code pushed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use Client";
-
 // import { Provider } from "react-redux";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
@@ -8,11 +6,9 @@ import Navbar from "./components/Navbar";
 export default function app() {
   return (
     <div>
-     
+      
       <Navbar />
       <HeroSection />
-   
-     
     </div>
   );
 }
